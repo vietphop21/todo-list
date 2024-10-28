4388e22b1c1a034494523d33ccfe9a48dabdcc23
Your Name <anhphop1@gmail.com>
2024-10-28 22:09:29 +0700

    test

---
+++ src/Header/header.js
@@ -0,0 +1,20 @@
+import Grid from "@mui/material/Grid";
+import { LOGO } from "../common";
+
+export const Header = () => {
+  return (
+    <>
+      <Grid container px={8} py={2} flexDirection="column" alignItems="center">
+        <Grid item xs={12}>
+          <img className="logo" src={LOGO} />
+        </Grid>
+
+        <Grid item xs={12}>
+          <strong style={{ color: "red", fontSize: "26px" }}>
+            QUỸ BẢO HIỂM CS
+          </strong>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
